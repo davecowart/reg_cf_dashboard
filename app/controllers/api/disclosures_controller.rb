46fae1a0707ed0345700a37948c9cd11832c3e47
Dave Cowart <davecowart@gmail.com>
2020-10-01 10:26:44 -0500

    set order

--- app/controllers/api/disclosures_controller.rb
+++ app/controllers/api/disclosures_controller.rb
@@ -3,14 +3,10 @@ class Api::DisclosuresController < ApplicationController
     limit = params[:pageSize].to_i || 50
     limit = 50 if limit == 0
     offset = limit * (params[:pageIndex].to_i || 0)
-    key = params[:orderBy] || 'accession_number'
-    direction = params[:orderDirection] == 'ASC' ? 'ASC' : 'DESC'
 
-    order = "#{key} #{direction}"
-    
     latest_ids = Disclosure.group(:accession_number_sub).maximum(:id).values
     output = {
-      disclosures: Disclosure.includes(:issuer).where(id: latest_ids).order(order).offset(offset).limit(limit).to_json(include: :issuer),
+      disclosures: Disclosure.includes(:issuer).where(id: latest_ids).order(year: :desc, quarter: :desc).offset(offset).limit(limit).to_json(include: :issuer),
       pages: latest_ids.count / limit
     }
     render json: output
